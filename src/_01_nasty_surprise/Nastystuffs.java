b9df90e4fe6d4e129f03ecafa983289514f24bb7
student <student@ip-172-31-53-197.us-west-2.compute.internal>
2021-05-08 16:26:48 -0700

    yes

--- src/_01_nasty_surprise/Nastystuffs.java
+++ src/_01_nasty_surprise/Nastystuffs.java
@@ -3,15 +3,25 @@ package _01_nasty_surprise;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 
-public class Nastystuffs {
-public static void main (String[]args) {
-	JButton goodbutton = new JButton();
-	JButton badbutton = new JButton();
+public class Nastystuffs implements ActionListener {
+JButton goodbutton = new JButton();
+JButton badbutton = new JButton();	
+
+
+	public void setup() {
+	
 	goodbutton.setSize(10, 10);
-	
-	
+	goodbutton.addActionListener(this);
+	badbutton.setSize(10, 10);
+	badbutton.addActionListener(this);
+	goodbutton.setText("Treat");
+	badbutton.setText("Trick");
 	
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
@@ -21,7 +31,27 @@ public static void main (String[]args) {
 	frame.setVisible(true);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	frame.pack();
+
+
+
+
+
 }
+
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		if(arg0.getSource()==badbutton) {
+			
+			
+		}else {
+			
+		}
+	}
 	
 	
 }
