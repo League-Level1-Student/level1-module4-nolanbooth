518dd862e11fbc618d0d5d678e21d9a79105559f
nolanbooth <56123783+nolanbooth@users.noreply.github.com>
2021-08-13 17:08:30 -0700

    Create Slot.java

---
+++ src/_12_slot_machine/Slot.java
@@ -0,0 +1,13 @@
+package _12_slot_machine;
+import javax.swing.*;
+import java.util.Random;
+public class Slot {
+
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	
+	
+	public void run() {
+		
+	}
+}
